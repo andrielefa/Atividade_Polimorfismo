da566dbcab3e68589b60d8c9bf2be5f98a3c9184
andrielefa <144621938+andrielefa@users.noreply.github.com>
2024-05-20 20:27:13 -0300

    onibus.java

--- onibus.java
+++ onibus.java
@@ -1,10 +1,6 @@
-public class Onibus extends veiculo{
-    public Onibus ( double precobase) {
-        super (precobase);
-    }
-    
-    @Override
-    protected double getPorcentagem(){
-        return 0.4;
+package Oficina;
+
+class Onibus extends Veiculo {
+    public Onibus() {
     }
 }
