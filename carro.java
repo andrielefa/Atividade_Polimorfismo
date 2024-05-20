5b685191cbc46aaf9e35f25a4ec6c9cfb0306112
andrielefa <144621938+andrielefa@users.noreply.github.com>
2024-05-20 20:25:27 -0300

    carro.java

--- carro.java
+++ carro.java
@@ -1,10 +1,26 @@
-public class Carro extends veiculo{
-    public Carro ( double precobase) {
-        super (precobase);
-    }
-    
-    @Override
-    protected double getPorcentagem(){
-        return 0.25;
+package Oficina;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira o valor das peças:");
+        double valorPecas = scanner.nextDouble();
+
+        Veiculo carro = new Carro();
+        Veiculo onibus = new Onibus();
+        Veiculo moto = new Moto();
+
+        double precoCarro = carro.calcularPrecoPiso(valorPecas, 25);
+        double precoOnibus = onibus.calcularPrecoPiso(valorPecas, 40);
+        double precoMoto = moto.calcularPrecoPiso(valorPecas, 15);
+
+        System.out.println("Preço das peças para o Carro: " + precoCarro);
+        System.out.println("Preço das peças para o Ônibus: " + precoOnibus);
+        System.out.println("Preço das peças para a Moto: " + precoMoto);
+
+        scanner.close();
     }
 }
